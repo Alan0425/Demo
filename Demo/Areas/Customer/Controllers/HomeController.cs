352befe85d30444c26611f76ebb986de52401e58
Alan0425 <spc0425@gmail.com>
2024-11-20 21:04:58 +0800

    Demo

---
+++ Demo/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,129 @@
+﻿using Demo.DataAccess.Repository.IRepository;
+using Demo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace Demo.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            //IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            //return View(productList);
+
+            return View();
+        }
+
+        public IActionResult Category(int categoryId)
+        {
+            TempData["category"] = categoryId.ToString();
+            return View();
+        }
+
+        public IActionResult Details(int productId)
+        {
+            ShoppingCart cart = new()
+            {
+                Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category"),
+                Count = 1,
+                ProductId = productId
+            };
+            return View(cart);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new { data = objProductList });
+        }
+
+        [HttpGet]
+        public IActionResult GetCategory(int category)
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(u => u.CategoryId == category, includeProperties: "Category").ToList();
+            return Json(new { data = objProductList });
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            shoppingCart.ApplicationUserId = userId;
+
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ApplicationUser.Id == userId && u.ProductId == shoppingCart.ProductId && u.Ice == shoppingCart.Ice && u.Sweetness == shoppingCart.Sweetness);
+            if (cartFromDb != null)
+            {
+                cartFromDb.Count += shoppingCart.Count;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+            }
+            //TempData["success"] = "加入購物車成功！";
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult AddDetail(IFormCollection formcollection)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.ApplicationUserId = userId;
+            shoppingCart.ProductId= Convert.ToInt32(formcollection["ProductId"]);
+            shoppingCart.Ice = formcollection["ice"].ToString();
+            shoppingCart.Sweetness = formcollection["sweet"].ToString();
+            shoppingCart.Count = Convert.ToInt32(formcollection["count"]);
+
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ApplicationUser.Id == userId && u.ProductId == shoppingCart.ProductId && u.Ice == shoppingCart.Ice && u.Sweetness == shoppingCart.Sweetness);
+            if (cartFromDb != null)
+            {
+                cartFromDb.Count += shoppingCart.Count;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+            }
+            //TempData["success"] = "加入購物車成功！";
+            _unitOfWork.Save();
+
+
+            return Json(new { success = true, message = "加入購物車成功" });
+
+            //return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Demo/wwwroot/js/list.js
@@ -0,0 +1,85 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            url: '/customer/home/getall'
+        },
+        "columns": [
+            { data: 'name', "width": "15%" },
+            { data: 'category.name', "width": "10%" },
+            { data: 'size', "width": "10%" },
+            { data: 'price', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<input type="radio" id="ice_${data}" name="ice_${data}" value="正常冰"/><label>正常冰</label>
+                    <input type="radio" id="ice_${data}" name="ice_${data}" value="少冰"/><label>少冰</label>
+                    <input type="radio" id="ice_${data}" name="ice_${data}" value="微冰"/><label>微冰</label>
+                    <input type="radio" id="ice_${data}" name="ice_${data}" value="去冰"/><label>去冰</label>
+                    <input type="radio" id="ice_${data}" name="ice_${data}" value="溫"/><label>溫</label>
+                    <input type="radio" id="ice_${data}" name="ice_${data}" value="熱"/><label>熱</label><br>
+                    <input type="radio" id="sweet_${data}" name="sweet_${data}" value="正常甜"/><label>正常甜</label>
+                    <input type="radio" id="sweet_${data}" name="sweet_${data}" value="少糖"/><label>少糖</label>
+                    <input type="radio" id="sweet_${data}" name="sweet_${data}" value="半糖"/><label>半糖</label>
+                    <input type="radio" id="sweet_${data}" name="sweet_${data}" value="微糖"/><label>微糖</label>
+                    <input type="radio" id="sweet_${data}" name="sweet_${data}" value="無糖"/><label>無糖</label>`
+                },
+                "width": "40%"
+            },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<input id="count_${data}" type="number" value="0" min="0">`
+                },
+                "width": "5%"
+            },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a onClick=Check('${data}') class="btn btn-primary mx-2"> <i class="bi bi-cart"></i> </a>
+                    </div>`
+                },
+                "width": "10%"
+            }
+        ]
+    });
+}
+
+function Check(id) {
+    $("#productId").val(id);
+    $("#ice").val($('input:radio:checked[name="ice_' + id + '"]').val());
+    $("#sweet").val($('input:radio:checked[name="sweet_' + id + '"]').val());
+    $("#count").val($("#count_" + id).val());
+
+    var error = "";
+    if ($("#ice").val() == "") error += "請選擇冰量 ";
+    if ($("#sweet").val() == "") error += " 請選擇甜度 ";
+    if ($("#count").val() == 0) error += " 請選擇數量 ";
+
+    if (error == "") Cart(id);
+    else alert(error);
+
+}
+
+function Cart(id) {
+    $.ajax({
+        url: '/customer/home/AddDetail',
+        type: 'POST',
+        data: jQuery.param({ ProductId: id, ice: $("#ice").val(), sweet: $("#sweet").val(), count: $("#count").val() }),
+        success: function (data) {
+            //alert(data.message);
+            //toastr.success(data.message);
+            $("#cartSpan").fadeIn(1000);
+            $("#cartSpan").fadeOut(1000);
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("請先登入");
+        }   
+    })
+}
+
