74111cecb31ed1a426be3930b4d03405f5c68b53
Alan0425 <spc0425@gmail.com>
2024-11-20 21:08:01 +0800

    Demo

---
+++ Demo.DataAccess/Data/DbContext.cs
@@ -0,0 +1,63 @@
+﻿using Demo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name="茶飲",DisplayOrder=1},
+                new Category { Id = 2, Name = "水果茶", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "咖啡", DisplayOrder = 3 }
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Name = "台灣水果茶",
+                    Size = "大杯",
+                    Description = "天然果飲，迷人多變。",
+                    Price = 60,
+                    CategoryId = 1,
+                    ImageUrl = ""
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "鐵觀音",
+                    Size = "中杯",
+                    Description = " 品鐵觀音，享人生的味道。",
+                    Price = 35,
+                    CategoryId = 2,
+                    ImageUrl = ""
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "冰美式咖啡",
+                    Size = "中杯",
+                    Description = "用咖啡體悟悠閒時光。",
+                    Price = 50,
+                    CategoryId = 3,
+                    ImageUrl = ""
+                }
+                );
+        }
+    }
+}
---
+++ Demo.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using Demo.DataAccess.Data;
+using Demo.DataAccess.Repository.IRepository;
+using Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            //_db.Products.Update(obj);
+
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = obj.Name;
+                objFromDb.Size = obj.Size;
+                objFromDb.Price = obj.Price;
+                objFromDb.Description = obj.Description;
+                objFromDb.CategoryId = obj.CategoryId;
+                if (objFromDb.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+        }
+    }
+}
